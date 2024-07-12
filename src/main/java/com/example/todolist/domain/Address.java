5f4b5e8a494f489223da6813b9482d3946125bb2
Manel <msoler92@gmail.com>
2024-07-12 14:42:51 +0200

    Wired main TODO List screen to database

--- src/main/java/com/example/todolist/domain/Address.java
+++ src/main/java/com/example/todolist/domain/Address.java
@@ -1,12 +1,12 @@
 package com.example.todolist.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
