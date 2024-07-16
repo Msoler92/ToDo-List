cc47cc7cb9e6950d1037377d4ca470eb524ed221
Manel <msoler92@gmail.com>
2024-07-16 21:35:21 +0200

    Modified update and delete Controller methods to not display id in the URL

--- src/main/java/com/example/todolist/controller/TodoController.java
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -44,7 +44,6 @@ public class TodoController {
         if (usernameFilter != null && usernameFilter.trim().isEmpty()) {
             usernameFilter = null;
         }
-        //TODO Allow asc and desc sorting
         pageResult = todoService.findAll(Optional.ofNullable(titleFilter), Optional.ofNullable(usernameFilter), pageRequest);
         List<Integer> pageIndex = IntStream.rangeClosed(1, pageResult.getTotalPages()).boxed().toList();
         model.addAttribute("todoPage", pageResult);
@@ -74,16 +73,16 @@ public class TodoController {
         return todoFormDto.getId() == 0 ? "redirect:/todo/todo" : "redirect:/todo/todos";
     }
 
-    @GetMapping("/todo/{id}")
-    public String getModificationForm(Model model, @PathVariable long id) {
+    @PostMapping("/todo/update")
+    public String getModificationForm(Model model, long id) {
         TodoFormDto todoFormDto = todoService.findById(id);
         model.addAttribute("userList", userService.findAll());
         model.addAttribute("todoFormDto", todoFormDto   );
         return TODO_FORM_SUBMISSION_PAGE;
     }
 
-    @PostMapping("/todo/{id}/delete")
-    public String deleteTodo(Model model, @PathVariable long id) {
+    @PostMapping("/todo/delete")
+    public String deleteTodo(Model model, long id) {
         todoService.delete(id);
         return "redirect:/todo/todos";
     }
