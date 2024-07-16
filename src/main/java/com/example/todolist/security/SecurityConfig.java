610a876db6d4b7fa95692115e9fc5ded0c2996e5
Manel <msoler92@gmail.com>
2024-07-16 22:27:10 +0200

    Updated outdated SecurityConfig matchers

--- src/main/java/com/example/todolist/security/SecurityConfig.java
+++ src/main/java/com/example/todolist/security/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/todo/todo/{id}","/todo/todo/{id}/delete").authenticated()
+                        .requestMatchers("/todo/todo/update","/todo/todo/delete").authenticated()
                         .anyRequest().permitAll()
                 )
                 .formLogin(login ->
